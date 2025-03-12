6f988c639afea277a4c1bd9ef4571a1488ff19a6
luchito <dillonlucio@gmail.com>
2025-03-12 15:45:21 -0300

    ej7

---
+++ Clase 1/Guia de Ejercicios/TP 6/ej7.py
@@ -0,0 +1,32 @@
+#Calcular y devolver el máximo comun divisor de dos enteros no negativos basándose en las siguientes formulas matemáticas
+#MCD(X,X) = X
+#MCD(X,Y) = MCD(Y,X)
+#Si X > Y => MCD(X, Y) = MCD(X-Y, Y)
+#Ejemplo: MCD(40, 15) devuelve 5
+
+def calcularMCD(n1, n2):
+    while n1 != n2:
+        if n1 > n2:
+            n1 = n1 - n2
+        else:
+            n2 = n2 - n1
+    return n1
+
+
+def main():
+    n1 = int(input("Ingrese el primer valor: "))
+    while n1 <= 0:
+        if n1 <= 0:
+            print("Ingrese un número válido. ")
+            n1 = int(input("Ingrese el primer valor: "))
+    
+    n2 = int(input("Ingrese el segundo valor: "))
+    while n1 <= 0:
+        if n2 <= 0:
+            print("Ingrese un número válido. ")
+            n2 = int(input("Ingrese el segundo valor: "))
+
+    resultado = calcularMCD(n1, n2)
+    print("El MCD de ", n1, "y ", n2, "es: ", resultado)
+
+main()
